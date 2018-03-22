457bd209999931f9d2a6cac2eb35a50cfe60cd66
gscienty <gaoxiaochuan@hotmail.com>
2018-03-22 22:23:39 +0800

    comp slow start unit test

--- test/slow_start_test.cc
+++ test/slow_start_test.cc
@@ -18,10 +18,38 @@ namespace kuic {
             EXPECT_FALSE(slowStart.isEndOfRound(packetNumber));
             packetNumber++;
             EXPECT_TRUE(slowStart.isEndOfRound(packetNumber));
+
+            endPacketNumber = 20;
+            slowStart.startReceiveRound(endPacketNumber);
+            while (packetNumber < endPacketNumber) {
+                packetNumber++;
+                EXPECT_FALSE(slowStart.isEndOfRound(packetNumber));
+            }
+
+            packetNumber++;
+            EXPECT_TRUE(slowStart.isEndOfRound(packetNumber));
         }
 
         TEST(slow_start, work_with_delay) {
-            
+            kuic::SlowStart slowStart;
+            long rtt = 60 * 1000 * 1000;
+            int minSamples = 8;
+
+            unsigned long endPacketNumber = 1UL;
+            endPacketNumber++;
+            slowStart.startReceiveRound(endPacketNumber);
+
+            for (int i = 0; i < minSamples; i++) {
+                EXPECT_FALSE(slowStart.shouldExitSlowStart(rtt + i * 1000 * 1000, rtt, 100));
+            }
+
+            endPacketNumber++;
+            slowStart.startReceiveRound(endPacketNumber);
+            for (int i = 1; i < minSamples; i++) {
+                EXPECT_FALSE(slowStart.shouldExitSlowStart(rtt + (i + 10) * 1000 * 1000, rtt, 100));
+            }
+
+            EXPECT_TRUE(slowStart.shouldExitSlowStart(rtt * 10 * 1000 * 1000, rtt, 100));
         }
     }
 }
