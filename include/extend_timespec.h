acb5ef7733a14373e27ffc26bac490ac9dde6ccf
Gscienty <gaoxiaochuan@hotmail.com>
2018-03-22 19:50:55 +0800

    change include

--- include/extend_timespec.h
+++ include/extend_timespec.h
@@ -1,7 +1,7 @@
 #ifndef _KUIC_TIMESPEC_
 #define _KUIC_TIMESPEC_
 
-#include <time.h>
+#include <sys/time.h>
 namespace kuic {
     timespec operator+ (const timespec& a, const timespec& b);
     timespec operator+ (const timespec& a, const long b);
