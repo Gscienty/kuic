4974e28f1a3ec7d93bbd8ffbd875eec08e4c0083
gscienty <gaoxiaochuan@hotmail.com>
2018-04-05 22:10:32 +0800

    add some extend timespec method

--- include/extend_timespec.h
+++ include/extend_timespec.h
@@ -6,12 +6,22 @@ namespace kuic {
     timespec operator+ (const timespec& a, const timespec& b);
     timespec operator+ (const timespec& a, const long b);
     timespec& operator+= (timespec& a, const long b);
+    timespec& operator+= (timespec& a, const timespec& b);
 
-    timespec operator- (const timespec& a, const timespec& b);
-    timespec operator- (const timespec& a, const long b);
+    long operator- (const timespec& a, const timespec& b);
 
     bool operator<= (const timespec& a, const long b);
     bool operator< (const timespec& a, const timespec& b);
+
+    class Clock {
+    public:
+        virtual timespec now() const = 0;
+    };
+
+    class DefaultClock : public Clock {
+    public:
+        timespec now() const;
+    };
 }
 
 #endif
