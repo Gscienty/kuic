5e7449278205dee678c4571e7d4d605f2a5d0730
gscienty <gaoxiaochuan@hotmail.com>
2018-05-01 01:23:32 +0800

    add kbr_key_version serializer & kbr_encryption_type serializer

--- include/handshake/serializer.h
+++ include/handshake/serializer.h
@@ -16,6 +16,16 @@ namespace kuic {
             }
         };
 
+        struct kbr_key_version_serializer {
+            static char *serialize(kuic::kbr_key_version_t e, size_t &size) {
+            return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            }
+
+            static kuic::kbr_key_version_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
+                return kuic::kbr_key_version_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            }
+        };
+
         struct kbr_message_type_serializer {
             static char *serialize(kuic::kbr_message_type_t e, size_t &size) {
                 return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
@@ -33,6 +43,16 @@ namespace kuic {
             
             static kuic::kbr_name_type_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
                 return kuic::kbr_name_type_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            }
+        };
+
+        struct kbr_encryption_type_serializer {
+            static char *serialize(kuic::kbr_encryption_type_t e, size_t &size) {
+                return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            }
+
+            static kuic::kbr_name_type_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
+                return kuic::kbr_encryption_type_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
             }
         };
     }
