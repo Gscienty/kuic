0241ab739dd149ae9a4d099c82ebb463fa9f3b5d
gscienty <gaoxiaochuan@hotmail.com>
2018-05-02 17:13:32 +0800

    change handshake base type serialize /deserialize method interface

--- include/handshake/serializer.h
+++ include/handshake/serializer.h
@@ -3,56 +3,57 @@
 
 #include "type.h"
 #include "eys.h"
+#include <utility>
 
 namespace kuic {
     namespace handshake {
         struct kbr_protocol_version_serializer {
-            static char *serialize(kuic::kbr_protocol_version_t e, size_t &size) {
-                return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            static std::pair<kuic::byte_t *, size_t> serialize(kuic::kbr_protocol_version_t e) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_protocol_version_t>::serialize(e);
             }
             
-            static kuic::kbr_protocol_version_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
-                return kuic::kbr_protocol_version_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            static kuic::kbr_protocol_version_t deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_protocol_version_t>::deserialize(buffer, len, seek);
             }
         };
 
         struct kbr_key_version_serializer {
-            static char *serialize(kuic::kbr_key_version_t e, size_t &size) {
-            return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            static std::pair<kuic::byte_t *, size_t> serialize(kuic::kbr_key_version_t e) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_key_version_t>::serialize(e);
             }
 
-            static kuic::kbr_key_version_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
-                return kuic::kbr_key_version_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            static kuic::kbr_key_version_t deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_key_version_t>::deserialize(buffer, len, seek);
             }
         };
 
         struct kbr_message_type_serializer {
-            static char *serialize(kuic::kbr_message_type_t e, size_t &size) {
-                return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            static std::pair<kuic::byte_t *, size_t> serialize(kuic::kbr_message_type_t e) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_message_type_t>::serialize(e);
             }
             
-            static kuic::kbr_message_type_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
-                return kuic::kbr_message_type_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            static kuic::kbr_message_type_t deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_message_type_t>::deserialize(buffer, len, seek);
             }
         };
 
         struct kbr_name_type_serializer {
-            static char *serialize(kuic::kbr_name_type_t e, size_t &size) {
-                return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            static std::pair<kuic::byte_t *, size_t> serialize(kuic::kbr_name_type_t e) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_name_type_t>::serialize(e);
             }
             
-            static kuic::kbr_name_type_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
-                return kuic::kbr_name_type_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            static kuic::kbr_name_type_t deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_name_type_t>::deserialize(buffer, len, seek);
             }
         };
 
         struct kbr_encryption_type_serializer {
-            static char *serialize(kuic::kbr_encryption_type_t e, size_t &size) {
-                return eys::serializer<unsigned int>::serialize((unsigned int)(e), size);
+            static std::pair<kuic::byte_t *, size_t> serialize(kuic::kbr_encryption_type_t e) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_encryption_type_t>::serialize(e);
             }
 
-            static kuic::kbr_name_type_t deserialize(const char *buffer, size_t len, ssize_t &seek) {
-                return kuic::kbr_encryption_type_t(eys::deserializer<unsigned int>::deserialize(buffer, len, seek));
+            static kuic::kbr_name_type_t deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+                return eys::bigendian_serializer<kuic::byte_t, kuic::kbr_encryption_type_t>::deserialize(buffer, len, seek);
             }
         };
     }
