f55f24ed14a74b75053e7916f0c5257dfd58f48a
gscienty <gaoxiaochuan@hotmail.com>
2018-05-22 01:20:50 +0800

    add stream frame

---
+++ include/frame/stream_frame.h
@@ -0,0 +1,35 @@
+#ifndef _KUIC_FRAME_STREAM_FRAME_
+#define _KUIC_FRAME_STREAM_FRAME_
+
+#include "frame/frame.h"
+#include "type.h"
+#include <vector>
+
+namespace kuic {
+    namespace frame {
+        class stream_frame
+            : public frame {
+        private:
+            kuic::stream_id_t stream_id;
+            bool fin_bit;
+            bool data_length_present;
+            kuic::bytes_count_t offset;
+            std::vector<kuic::byte_t> data;
+
+            stream_frame() : data(nullptr) { }
+            stream_frame(kuic::error_t error) : lawful_package(error) { }
+        public:
+            static stream_frame deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek);
+
+            virtual size_t length() const override;
+            virtual kuic::frame_type_t type() const override;
+            virtual std::pair<kuic::byte_t *, size_t> serialize() const override;
+
+            kuic::bytes_count_t max_data_length(kuic::bytes_count_t max_size) const;
+            stream_frame maybe_split_offset_frame(kuic::bytes_count_t max_size);
+        };
+    }
+}
+
+#endif
+
---
+++ src/frame/stream_frame.cc
@@ -0,0 +1,137 @@
+#include "frame/stream_frame.h"
+#include "variable_integer.h"
+#include <algorithm>
+
+kuic::frame::stream_frame
+kuic::frame::stream_frame::deserialize(const kuic::byte_t *buffer, size_t len, size_t &seek) {
+    kuic::frame::stream_frame frame;
+
+    kuic::byte_t type_byte = buffer[seek++]; // ignore type
+    if (seek >= len) {
+        return kuic::frame::stream_frame(kuic::reader_buffer_remain_not_enough);
+    }
+
+    frame.fin_bit = (type_byte & 0x01) > 0;
+    frame.data_length_present = (type_byte & 0x02) > 0;
+    bool has_offset = (type_byte & 0x04) > 0;
+
+    frame.stream_id = kuic::variable_integer::read(buffer, len, seek);
+
+    if (has_offset) {
+        frame.offset = kuic::variable_integer::read(buffer, len, seek);
+    }
+    else {
+        frame.offset = 0;
+    }
+
+    kuic::bytes_count_t data_length = 0;
+    if (frame.data_length_present) {
+        data_length = kuic::variable_integer::read(buffer, len, seek);
+        if (data_length > len - seek) {
+            return kuic::frame::stream_frame(kuic::reader_buffer_remain_not_enough);
+        }
+    }
+    else {
+        data_length = len - seek;
+    }
+    
+    if (data_length != 0) {
+        frame.data.assign(buffer + seek, buffer + seek + data_length);
+        seek += data_length;
+    }
+
+    return frame;
+}
+
+std::pair<kuic::byte_t *, size_t>
+kuic::frame::stream_frame::serialize() const {
+    if (this->data.empty() && this->fin_bit == false) {
+        return std::pair<kuic::byte_t *, size_t>(nullptr, 0);
+    }
+
+    size_t size = this->length();
+    size_t seek = 0;
+    kuic::byte_t *result = new kuic::byte_t[size];
+
+    kuic::byte_t type_byte = 0x10;
+
+    if (this->fin_bit) {
+        type_byte ^= 0x01;
+    }
+    if (this->data_length_present) {
+        type_byte ^= 0x02;
+    }
+    if (this->offset != 0) {
+        type_byte ^= 0x04;
+    }
+
+    result[seek++] = type_byte;
+    kuic::frame::frame::fill(result, size, seek, kuic::variable_integer::write(this->stream_id));
+    if (this->offset != 0) {
+        kuic::frame::frame::fill(result, size, seek, kuic::variable_integer::write(this->offset));
+    }
+    if (this->data_length_present) {
+        kuic::frame::frame::fill(result, size, seek, kuic::variable_integer::write(this->data.size()));
+    }
+
+    std::copy(this->data.begin(), this->data.end(), result + seek);
+
+    return std::pair<kuic::byte_t *, size_t>(result, size);
+}
+
+size_t kuic::frame::stream_frame::length() const {
+    size_t length = 1 + kuic::variable_integer::length(this->stream_id);
+    if (this->offset != 0) {
+        length += kuic::variable_integer::length(this->offset);
+    }
+    if (this->data_length_present) {
+        length += kuic::variable_integer::length(this->data.size());
+    }
+
+    return length + this->data.size();
+}
+
+kuic::bytes_count_t
+kuic::frame::stream_frame::max_data_length(kuic::bytes_count_t max_size) const {
+    size_t header_length = 1 + kuic::variable_integer::length(this->stream_id);
+    if (this->offset != 0) {
+        header_length += kuic::variable_integer::length(this->offset);
+    }
+    if (this->data_length_present) {
+        header_length += kuic::variable_integer::length(this->data.size());
+    }
+    if (header_length > max_size) {
+        return 0;
+    }
+
+    kuic::bytes_count_t max_data_length = max_size - header_length;
+    if (this->data_length_present && kuic::variable_integer::length(max_data_length) != 1) {
+        max_data_length--;
+    }
+
+    return max_data_length;
+}
+
+kuic::frame::stream_frame
+kuic::frame::stream_frame::maybe_split_offset_frame(kuic::bytes_count_t max_size) {
+    if (max_size >= this->length()) {
+        return kuic::frame::stream_frame(kuic::not_expect);
+    }
+
+    kuic::bytes_count_t n = this->max_data_length(max_size);
+    if (n == 0) {
+        return kuic::frame::stream_frame(kuic::not_expect);
+    }
+
+    kuic::frame::stream_frame new_frame;
+    new_frame.fin_bit = false;
+    new_frame.stream_id = this->stream_id;
+    new_frame.offset = this->offset;
+    new_frame.data.assign(this->data.begin(), this->data.begin() + n);
+    new_frame.data_length_present = this->data_length_present;
+
+    this->data.assign(this->data.begin() + n, this->data.end());
+    this->offset += n;
+
+    return new_frame;
+}
