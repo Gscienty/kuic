4dc7a8d983646e0cbd6b34be6de4d19e7502f586
gscienty <gaoxiaochuan@hotmail.com>
2018-05-23 03:32:20 +0800

    add ref

--- include/nullable.h
+++ include/nullable.h
@@ -6,35 +6,50 @@
 namespace kuic {
     template <typename ClassType> class nullable {
     private:
+        bool is_reference;
         ClassType *ptr;
     public:
-        nullable(std::nullptr_t) noexcept : ptr(nullptr) { }
-        template<typename ClassType_1> nullable(ClassType_1 *p = nullptr) noexcept : ptr(p) { }
-        template<typename ClassType_1> nullable(ClassType_1 &instance) noexcept : ptr(&instance) { }
-        template<typename ClassType_1> nullable(nullable<ClassType_1> &nullable_instance) : ptr(nullable_instance.ptr) { };
+        nullable(std::nullptr_t) noexcept : is_reference(false), ptr(nullptr) { }
+        template<typename ClassType_1> nullable(ClassType_1 *p = nullptr) noexcept : is_reference(false), ptr(p) { }
+        template<typename ClassType_1> nullable(ClassType_1 &instance) noexcept : is_reference(true), ptr(&instance) { }
+        template<typename ClassType_1> nullable(nullable<ClassType_1> &nullable_instance) 
+            : is_reference(nullable_instance.is_reference), ptr(nullable_instance.ptr) { };
         
-        ~nullable() { delete ptr; }
+        ~nullable() { 
+            if (this->is_reference == false && this->ptr != nullptr) {
+                delete ptr;
+            }
+        }
 
         template<typename ClassType_1> nullable<ClassType> &operator= (nullable<ClassType_1> &a) noexcept {
             if (&a != this) {
-                delete this->ptr;
+                if (this->is_reference == false) {
+                    delete this->ptr;
+                }
                 this->ptr = a.release();
+                this->is_reference = a.is_reference;
             }
             return *this;
         }
 
         nullable &operator= (nullable<ClassType> &a) noexcept {
             if (&a != this) {
-                delete this->ptr;
+                if (this->is_reference == false) {
+                    delete this->ptr;
+                }
                 this->ptr = a.release();
+                this->is_reference = a.is_reference;
             }
             return *this;
         }
 
         nullable &operator= (ClassType *a) noexcept {
             if (a != this->ptr) {
-                delete this->ptr;
+                if (this->is_reference == false) {
+                    delete this->ptr;
+                }
                 this->ptr = a;
+                this->is_reference = a->is_reference;
             }
             return *this;
         }
@@ -63,8 +78,11 @@ namespace kuic {
 
         void reset(ClassType *p = nullptr) noexcept {
             if (p != this->ptr) {
-                delete this->ptr;
+                if (this->is_reference == false) {
+                    delete this->ptr;
+                }
                 this->ptr = p;
+                this->is_reference = false;
             }
         }
     };
