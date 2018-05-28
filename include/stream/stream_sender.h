05aee80aeba412997611514dcc6407fdd4635acd
gscienty <gaoxiaochuan@hotmail.com>
2018-05-28 22:43:20 +0800

    change unicast sender stru

--- include/stream/stream_sender.h
+++ include/stream/stream_sender.h
@@ -16,9 +16,24 @@ namespace kuic {
         class unicast_stream_sender
             : public stream_sender {
         private:
+            stream_sender &origin_sender;
             std::function<void ()> on_stream_completed_implement;
         public:
-            virtual void on_stream_completed() override {
+            unicast_stream_sender(
+                    stream_sender &stream_sender,
+                    std::function<void ()> on_stream_completed_implement)
+                : origin_sender(stream_sender)
+                , on_stream_completed_implement(on_stream_completed_implement) { }
+
+            virtual void queue_control_frame(kuic::frame::frame &frame) override {
+                this->origin_sender.queue_control_frame(frame);
+            }
+
+            virtual void on_has_stream_data(kuic::stream_id_t stream_id) override {
+                this->origin_sender.on_has_stream_data(stream_id);
+            }
+
+            virtual void on_stream_completed(kuic::stream_id_t) override {
                 this->on_stream_completed_implement();
             }
         };
