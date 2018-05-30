f66b724d89a6de185b03313a48c7f729aab8bec3
gscienty <gaoxiaochuan@hotmail.com>
2018-05-31 02:24:57 +0800

    change ctor

--- src/stream/streams_map.cc
+++ src/stream/streams_map.cc
@@ -18,12 +18,20 @@ kuic::stream::streams_map::streams_map(
                 this->sender.queue_control_frame(frame);
             },
             [this] (kuic::stream_id_t stream_id) -> kuic::stream::receive_stream * {
-                return new kuic::stream::receive_stream(stream_id, this->sender, this->new_flow_controller(stream_id));
+                return new kuic::stream::receive_stream(
+                        stream_id,
+                        this->sender,
+                        std::make_shared<kuic::flowcontrol::stream_flow_controller>(
+                            this->new_flow_controller(stream_id)));
             })
     , out_uni(
             is_client ? 2 : 3,
             [this] (kuic::stream_id_t stream_id) -> kuic::stream::send_stream * {
-                return new kuic::stream::send_stream(stream_id, this->sender, this->new_flow_controller(stream_id));
+                return new kuic::stream::send_stream(
+                        stream_id,
+                        this->sender,
+                        std::make_shared<kuic::flowcontrol::stream_flow_controller>(
+                            this->new_flow_controller(stream_id)));
             },
             [this] (kuic::frame::stream_id_blocked_frame &frame) -> void {
                 this->sender.queue_control_frame(frame);
