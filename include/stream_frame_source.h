b57bd1e3268841cf7f4f791cfeb8ffd05f4d2b38
gscienty <gaoxiaochuan@hotmail.com>
2018-06-11 18:05:13 +0800

    add stream frame source

---
+++ include/stream_frame_source.h
@@ -0,0 +1,19 @@
+#ifndef _KUIC_STREAM_FRAME_SOURCE_
+#define _KUIC_STREAM_FRAME_SOURCE_
+
+#include "frame/stream_frame.h"
+#include "type.h"
+#include "nullable.h"
+#include <list>
+
+namespace kuic {
+    class stream_frame_source {
+    public:
+        virtual bool get_has_crypto_stream_data() = 0;
+        virtual kuic::frame::stream_frame &pop_crypto_stream_frame(kuic::bytes_count_t max_len) = 0;
+        virtual std::list<kuic::nullable<kuic::frame::stream_frame>> pop_stream_frames(kuic::bytes_count_t max_total_len) = 0;
+    };
+}
+
+#endif
+
