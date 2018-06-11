7ba9a286979d6bceb4c4dbe8e6867ed700724e93
gscienty <gaoxiaochuan@hotmail.com>
2018-06-11 18:24:13 +0800

    add field getter

--- include/packed_packet.h
+++ include/packed_packet.h
@@ -5,16 +5,21 @@
 #include "frame/frame.h"
 #include "ackhandler/packet.h"
 #include "type.h"
-#include <vector>
+#include <string>
+#include <memory>
 
 namespace kuic {
     class packed_packet {
     private:
-        kuic::frame::header header;
-        std::vector<kuic::byte_t> raw;
+        std::shared_ptr<kuic::frame::header> header;
+        std::string raw;
         std::vector<std::shared_ptr<kuic::frame::frame>> frames;
         bool is_handshake;
     public:
+        std::shared_ptr<kuic::frame::header> &get_header();
+        std::string &get_raw();
+        std::vector<std::shared_ptr<kuic::frame::frame>> &get_frames();
+        bool &get_is_handshake();
         kuic::ackhandler::packet to_ack_handler_packet();
     };
 }
