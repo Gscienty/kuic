efe776490023a67a2f1b3284655e7f0b5c3723cf
gscienty <gaoxiaochuan@hotmail.com>
2018-06-11 18:25:46 +0800

    impl packed packet

--- src/packed_packet.cc
+++ src/packed_packet.cc
@@ -3,8 +3,8 @@
 kuic::ackhandler::packet
 kuic::packed_packet::to_ack_handler_packet() {
     kuic::ackhandler::packet packet;
-    packet.packet_number = this->header.get_packet_number();
-    packet.packet_type = this->header.get_packet_type();
+    packet.packet_number = this->header->get_packet_number();
+    packet.packet_type = this->header->get_packet_type();
     packet.frames = this->frames;
     packet.length = this->raw.size();
     packet.is_handshake = this->is_handshake;
@@ -13,4 +13,23 @@ kuic::packed_packet::to_ack_handler_packet() {
     return packet;
 }
 
+std::shared_ptr<kuic::frame::header> &
+kuic::packed_packet::get_header() {
+    return this->header;
+}
+
+std::string &
+kuic::packed_packet::get_raw() {
+    return this->raw;
+}
+
+std::vector<std::shared_ptr<kuic::frame::frame>> &
+kuic::packed_packet::get_frames() {
+    return this->frames;
+}
+
+bool &
+kuic::packed_packet::get_is_handshake() {
+    return this->is_handshake;
+}
 
